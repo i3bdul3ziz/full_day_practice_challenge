964665b1818c5c534492c5d9426f681b985e8dbb
Abdulaziz Althagafi <labdul2ziz@gmail.com>
2020-03-26 11:12:05 +0300

    add items route

--- routes/items.route.js
+++ routes/items.route.js
@@ -5,6 +5,30 @@ const User = require('../models/user.model')
 const Item = require('../models/items.model')
 
 router.get('/:id/items', (req, res) =>{
-    res.render('items')
+    User.findById(req.params.id).then(item => {
+        res.render('items/index')
+    })
 })
 
+
+router.get('/:id/items/create', (req, res) =>{
+    User.findById(req.params.id).then(item => {
+        res.render('items/create')
+    })
+})
+
+router.post('/:id/items/create', (req, res) =>{
+    User.findById(req.params.id).then(() => {
+    let item = new Item(req.body)
+    // save item
+    item
+        .save()
+        .then(() => {
+        res.redirect('/:id/items')
+        })
+        .catch( err => {
+            console.log(err)
+            res.send('Error!!!!!!')
+        })
+    })
+})
